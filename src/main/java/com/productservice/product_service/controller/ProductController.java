656ea8116440476ef7e7eed26830ad21c8544019
RAI <aman.rai@capgemini.com>
2025-07-23 15:07:07 +0530

    remove unwanted import file

--- src/main/java/com/productservice/product_service/controller/ProductController.java
+++ src/main/java/com/productservice/product_service/controller/ProductController.java
@@ -5,7 +5,6 @@ import com.productservice.product_service.model.ProductDTO;
 import com.productservice.product_service.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
